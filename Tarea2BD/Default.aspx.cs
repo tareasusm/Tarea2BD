511abb589cbc54cf6d7b4a198bcf5e759e80f715
mario <mario@.(none)>
2010-10-10 03:26:48 -0300

    asdas

--- Tarea2BD/Default.aspx.cs
+++ Tarea2BD/Default.aspx.cs
@@ -18,10 +18,7 @@ namespace Tarea2BD
         protected void Page_Load(object sender, EventArgs e)
         {
  		    int i; 
-            asda
 
-                aas
-            asda
         }
     }
 }
