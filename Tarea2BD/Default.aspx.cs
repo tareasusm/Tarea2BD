030cc6492973b7ad5b44ddf95dc0779d988742ce
Mario <mgarces132@gmail.com>
2010-10-10 01:44:07 -0300

    asdasd

--- Tarea2BD/Default.aspx.cs
+++ Tarea2BD/Default.aspx.cs
@@ -18,7 +18,8 @@ namespace Tarea2BD
         protected void Page_Load(object sender, EventArgs e)
         {
  		    int i;
-
+            i = 3;
+            System.OutOfMemoryException;
         }
     }
 }
